f68416ab45a46d1aae7bb92fde494cc95c0a4e83
JustinChou <justinchou186@gmail.com>
2016-09-26 11:44:03 +0800

    初始化项目

---
+++ ScrollNumber/ScrollNumber/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollNumber
+//
+//  Created by JustinChou on 16/9/26.
+//  Copyright © 2016年 JustinChou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollNumber/ScrollNumber/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollNumber
+//
+//  Created by JustinChou on 16/9/26.
+//  Copyright © 2016年 JustinChou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
